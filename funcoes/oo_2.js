685bfd0172aa04a302a7a80dec9ebededa4e1a2b
RafaelRNG <rafael.gomila@hotmail.com>
2021-02-28 04:50:18 -0300

    OO no javascript

---
+++ funcoes/oo_2.js
@@ -0,0 +1,25 @@
+class Produto {
+
+   constructor(nome, preco, desc) {
+      this._nome = nome;
+      this.preco = preco;
+      this.desc = desc;
+   }
+
+   get nome() {
+      return `Produto: ${this._nome};`
+   }
+
+   set nome(novoNome) {
+      this._nome = novoNome.toUpperCase();
+   }
+
+   get precoFinal() {
+      return this.preco * (1 - this.desc)
+   }
+}
+
+const p1 = new Produto("caneta", 2.75, 0.05);
+p1.nome = "caneta";
+console.log(p1.nome);
+console.log(p1.precoFinal)
