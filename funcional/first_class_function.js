4bd10ea0d817a866e0cef9cd8cf8184cd3dce02f
RafaelRNG <rafael.gomila@hotmail.com>
2021-03-02 01:06:15 -0300

    Higher Order Funcion e First Class Function

---
+++ funcional/first_class_function.js
@@ -0,0 +1,15 @@
+/**
+ * Diz-se uma linguagem de programação possui
+ * funções de primeira classe quando funções nessa
+ * linguagem são tratadas como qualquer outra variável.
+ */
+
+const x = 3
+const y = function (txt) {
+  return `Esse é o texto: ${txt}`
+}
+
+const z = () => console.log("Zzzzzz")
+console.log(x)
+console.log(y("olá"))
+z()
